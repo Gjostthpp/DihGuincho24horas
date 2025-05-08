c758f76db45b179c9a919b27e66c24b95efed001
Gjostthpp <128274841+Gjostthpp@users.noreply.github.com>
2025-05-08 00:26:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let currentIndex = 0;
+        const images = document.querySelectorAll(".carousel-image");
+
+        function showImage(index) {
+            images.forEach((img, i) => {
+                img.classList.remove("active");
+                if (i === index) {
+                    img.classList.add("active");
+                }
+            });
+        }
+
+        function nextImage() {
+            currentIndex = (currentIndex + 1) % images.length;
+            showImage(currentIndex);
+        }
+
+        showImage(currentIndex);
+        setInterval(nextImage, 5000);
